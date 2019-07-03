b5c10e714e830030a3ba2aa6f177755f013dcd27
BCDev <bcdev@192-168-1-103.tpgi.com.au>
2019-07-03 11:53:58 +1000

    Grouping functions into classes and objects. Adding import directive tracing

--- sol2umlCLI.js
+++ sol2umlCLI.js
@@ -5,26 +5,36 @@ const solidity2plantuml = require("./index.js");
 
 
 
-function sol2umlFolder(path = ".", showFilenameAsPackage = false ){
+function sol2umlFolder(path = ".", showFilenameAsPackage = false, sol2uml ){
 	if(fs.statSync(path).isDirectory()){
 		var dir = fs.readdirSync(path);
 		for(var f in dir){
 			if(dir[f].slice(0,1) != ".")
-				sol2umlFolder(path + "/" + dir[f]);
+				console.log(dir[f]);
+				sol2umlFolder(path + "/" + dir[f], showFilenameAsPackage, sol2uml);
 		}
 	}else if(path.endsWith(".sol")){
-		// var p = path.replace("/Volumes/DOCS/deveth/2Solidity/oz/openzeppelin-solidity/contracts/","");
-		var startDecorator = showFilenameAsPackage?`package ${path} {
-			`:"";
-		var endDecorator = showFilenameAsPackage?"\n}":"";
+		var p = showFilenameAsPackage?path.split("/").pop():"";
 		var file = fs.readFileSync(path, "utf8");
-		// eslint-disable-next-line no-console
-		console.log(solidity2plantuml(file, startDecorator, endDecorator)); 
-		// future version should display files as packages e.g.: solidity2plantuml(file, "", "\n}" ));
+		sol2uml.importSolidity(file, p);
 	}
 }
-// sol2umlFolder("/Volumes/DOCS/deveth/2Solidity/oz/openzeppelin-solidity/contracts/access/");
 
+function sol2umlInheritance(path = ".", showFilenameAsPackage = false, sol2uml ){
+	if(path.endsWith(".sol")){
+		var packageName = showFilenameAsPackage?path.split("/").pop():"";
+		var file = fs.readFileSync(path, "utf8");
+		// eslint-disable-next-line no-console
+		sol2uml.importSolidity(file, packageName);
+		var importDirectives = sol2uml.collectImportDirectives(file, path);
+		var importDirectivesIter = Object.keys(importDirectives);
+		for(var i in importDirectivesIter){
+			if(!importDirectivesIter[i].startsWith("http")){
+				sol2umlInheritance(importDirectivesIter[i], showFilenameAsPackage, sol2uml);
+			}
+		}
+	}
+}
 
 
 
@@ -35,9 +45,13 @@ function sol2umlCLI(){
 	}else if(process.argv.length == 3){
 		if(process.argv[2].endsWith("help")||process.argv[2] == "-h"){
 			// eslint-disable-next-line no-console
+			console.log("node sol2umlCLI [/path/to/somefile.sol]");
 			console.log("node sol2umlCLI [/path/to/folder/containing/somefiles.sol]");
+			console.log("If you provide a file as an argument the converter will trace all import directives. If you provide a folder all files in the folder will be processed.")
 		}else{
-			sol2umlFolder(process.argv[2]);
+			var s2u = new solidity2plantuml.Sol2UML();
+			sol2umlInheritance(process.argv[2], true, s2u);
+			console.log(s2u.toString());
 		}
 	}else if(process.argv.length == 4){
 		// eslint-disable-next-line no-extra-semi
